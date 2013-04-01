411f0dadb408a58d547f53aea3d0c93976f2705f
Aaron Lebahn <cplusplus328@gmail.com>
2013-03-31 18:41:03 -0600

    Initial commit of rails and whatnot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssignmentSentinel::Application.config.secret_token = '72c10afbd63f557c31e79f309c92eb9009d207923b84964554dd0f38810818d090875a4c6bbd0552a10f9dec2d66ba7e942fc40c76dfca979073938210aa91ed'
