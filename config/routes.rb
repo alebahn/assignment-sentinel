2b43b72d2132afb3392e34512abdb0ed6713ed2b
Aaron Lebahn <cplusplus328@gmail.com>
2013-03-31 20:03:24 -0600

    Generated scaffold for users and ran db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 AssignmentSentinel::Application.routes.draw do
 
+  resources :users
+
+
   root :to => 'pages#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
