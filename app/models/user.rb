338bb3a94b86b090fa5a2de016ce1b1034b3a780
Aaron Lebahn <cplusplus328@gmail.com>
2013-04-01 23:05:00 -0600

    Added paperclip. Added avatar using paperclip. Ran migrate.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :password, :password_confirmation, :password_digest
+  attr_accessible :name, :password, :password_confirmation, :password_digest, :avatar
   validates :name, presence: true, uniqueness: true
   has_secure_password
+  has_attached_file :avatar, :styles => { :medium => "300x300>" }, :default_url => "/images/:style/default.jpg"
 end
