cd25116c0909910f40b4fd5fb60b5b83ff7c655a
Aaron Lebahn <cplusplus328@gmail.com>
2013-04-01 21:58:24 -0600

    fixed vulnerability where user could post to update user and modify a user other than himself

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
+    @user = User.find(session[:user_id])
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
