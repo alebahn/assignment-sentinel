64cb91bfdb00c05c6491b74ff5b14a5f4caa79c5
Aaron Lebahn <cplusplus328@gmail.com>
2013-04-15 01:22:42 -0600

    Added code to prevent deleting clases users are in. Removed buttons to manipulate the whole course from the My courses page. Changed header of my courses.

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,7 +8,20 @@ class Course < ActiveRecord::Base
   validates :name, presence: true
   validates :number, presence: true, numericality: {only_integer: true, greater_than: 0}
 
+  before_destroy :ensure_not_referenced_by_any_user
+
   def course_number
     department.abbr + " " + number.to_s
   end
+
+  private
+    #ensure that there are no courses referencing this department
+    def ensure_not_referenced_by_any_user
+      if users.empty?
+        return true
+      else
+        errors.add(:base, 'Courses present')
+        return false
+      end
+    end
 end
