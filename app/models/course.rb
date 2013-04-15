d771762d374e01ad2e3b193d702ccccd240566d5
Aaron Lebahn <cplusplus328@gmail.com>
2013-04-14 21:36:30 -0600

    Added ability to create valid Departments and Courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,9 @@
 class Course < ActiveRecord::Base
   attr_accessible :department_id, :name, :number
+
+  belongs_to :department
+
+  validates :department, presence: true
+  validates :name, presence: true
+  validates :number, presence: true, numericality: {only_integer: true, greater_than: 0}
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,8 @@
 class Department < ActiveRecord::Base
   attr_accessible :abbr, :name
+
+  has_many :courses
+
+  validates :name, presence: true
+  validates :abbr, presence: true, format: { with: /\A[A-Z]{4}\z/}
 end
