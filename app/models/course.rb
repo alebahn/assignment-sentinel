4caccaeac782b603fa394fb2d076b3190201747d
Aaron Lebahn <cplusplus328@gmail.com>
2013-04-14 22:00:28 -0600

    Added validation for removing Departments. Listing for Courses reads better.

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,4 +6,8 @@ class Course < ActiveRecord::Base
   validates :department, presence: true
   validates :name, presence: true
   validates :number, presence: true, numericality: {only_integer: true, greater_than: 0}
+
+  def course_number
+    department.abbr + " " + number.to_s
+  end
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -5,4 +5,17 @@ class Department < ActiveRecord::Base
 
   validates :name, presence: true
   validates :abbr, presence: true, format: { with: /\A[A-Z]{4}\z/}
+
+  before_destroy :ensure_not_referenced_by_any_course
+
+  private
+    #ensure that there are no courses referencing this department
+    def ensure_not_referenced_by_any_course
+      if courses.empty?
+        return true
+      else
+        errors.add(:base, 'Courses present')
+        return false
+      end
+    end
 end
